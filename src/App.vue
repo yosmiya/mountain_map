43a7e3482c17ad6cb4907a6908a656385c415c33
yosmiya <gigongben@gmail.com>
2021-08-15 07:23:01 +0900

    レイアウト変更

--- src/App.vue
+++ src/App.vue
@@ -70,6 +70,14 @@ export default defineComponent({
             return data.elevation >= 1000 && data.elevation <= 1500;
           case "3":
             return data.elevation >= 1500 && data.elevation <= 2000;
+          case "4":
+            return data.elevation >= 2000 && data.elevation <= 2500;
+          case "5":
+            return data.elevation >= 2500 && data.elevation <= 3000;
+          case "6":
+            return data.elevation >= 3000 && data.elevation <= 3500;
+          case "7":
+            return data.elevation >= 3500 && data.elevation <= 4000;
           default:
             return data.elevation >= 0 && data.elevation <= 500;
         }
@@ -128,6 +136,11 @@ export default defineComponent({
       });
     }
 
+    const onClickSearchButton = () => {
+      hideMarkers(); 
+      addMarker();
+    }
+
     onMounted(async () => {
       state.google = await GoogleMapsApiLoader({
         apiKey: GOOGLEMAP_API_KEY
@@ -150,31 +163,99 @@ export default defineComponent({
       hideMarkers,
       addMarker,
       clickMountainList,
-      openInfoWindow
-
+      openInfoWindow,
+      onClickSearchButton
     };
   }
 });
 </script>
 
 <template>
-  <div>
+  <div class="mainContent">
+
     <div class="searchContent">
-      <div>
-        <span>標高(m)</span>
+      <div class="searchBlock">
+        <span class="searchBlock_title">標高(m)</span>
         <br>
+        <!-- <br>
         <input type="radio" id="one" value="1" v-model="state.mountain_id" v-on:change="hideMarkers(); addMarker()" /><label for="one">500 - 1000</label>
         <br>
         <input type="radio" id="two" value="2" v-model="state.mountain_id" v-on:change="hideMarkers(); addMarker()" /><label for="two">1000 - 1500</label>
         <br>
-        <input type="radio" id="three" value="3" v-model="state.mountain_id" v-on:change="hideMarkers(); addMarker()" /><label for="three">1500 - 2000</label>
+        <input type="radio" id="three" value="3" v-model="state.mountain_id" v-on:change="hideMarkers(); addMarker()" /><label for="three">1500 - 2000</label> -->
+        <select v-model="state.mountain_id">
+          <option value="1">500 - 1000</option>
+          <option value="2">1000 - 1500</option>
+          <option value="3">1500 - 2000</option>
+          <option value="4">2000 - 2500</option>
+          <option value="5">2500 - 3000</option>
+          <option value="6">3000 - 3500</option>
+          <option value="7">3500 - 4000</option>
+        </select>
       </div>
+      <div class="searchBlock">
+        <span class="searchBlock_title">都道府県</span>
+        <br>
+        <select name="ken">
+          <option value="選択してください">-</option>
+          <option value="北海道">北海道</option>
+          <option value="青森県">青森県</option>
+          <option value="岩手県">岩手県</option>
+          <option value="宮城県">宮城県</option>
+          <option value="秋田県">秋田県</option>
+          <option value="山形県">山形県</option>
+          <option value="福島県">福島県</option>
+          <option value="茨城県">茨城県</option>
+          <option value="栃木県">栃木県</option>
+          <option value="群馬県">群馬県</option>
+          <option value="埼玉県">埼玉県</option>
+          <option value="千葉県">千葉県</option>
+          <option value="東京都">東京都</option>
+          <option value="神奈川県">神奈川県</option>
+          <option value="新潟県">新潟県</option>
+          <option value="富山県">富山県</option>
+          <option value="石川県">石川県</option>
+          <option value="福井県">福井県</option>
+          <option value="山梨県">山梨県</option>
+          <option value="長野県">長野県</option>
+          <option value="岐阜県">岐阜県</option>
+          <option value="静岡県">静岡県</option>
+          <option value="愛知県">愛知県</option>
+          <option value="三重県">三重県</option>
+          <option value="滋賀県">滋賀県</option>
+          <option value="京都府">京都府</option>
+          <option value="大阪府">大阪府</option>
+          <option value="兵庫県">兵庫県</option>
+          <option value="奈良県">奈良県</option>
+          <option value="和歌山県">和歌山県</option>
+          <option value="鳥取県">鳥取県</option>
+          <option value="島根県">島根県</option>
+          <option value="岡山県">岡山県</option>
+          <option value="広島県">広島県</option>
+          <option value="山口県">山口県</option>
+          <option value="徳島県">徳島県</option>
+          <option value="香川県">香川県</option>
+          <option value="愛媛県">愛媛県</option>
+          <option value="高知県">高知県</option>
+          <option value="福岡県">福岡県</option>
+          <option value="佐賀県">佐賀県</option>
+          <option value="長崎県">長崎県</option>
+          <option value="熊本県">熊本県</option>
+          <option value="大分県">大分県</option>
+          <option value="宮崎県">宮崎県</option>
+          <option value="鹿児島県">鹿児島県</option>
+          <option value="沖縄県">沖縄県</option>
+        </select>
+      </div>
+      <div class="searchBlock">
+        <input type="button" value="検索する" @click="onClickSearchButton">
+      </div>
+
       <!-- <div>
         <button v-on:click="hideMarkers">マーカー全削除</button>
       </div> -->
     </div>
-  </div>
-  <div class="mainContent">
+
     <div class="mountainList">
       <div class="mountainList_data" v-for="data in state.mountainData" :key="data.id" v-on:click="clickMountainList(data)">
         <span class="mountainList_data_name"><b>{{ data.name }}</b>（{{ data.kana }}）</span>
@@ -182,8 +263,11 @@ export default defineComponent({
         <span>標高：{{ data.elevation }}m 場所：{{ data.prefectures }} {{ data.location }}</span>
       </div>
     </div>
+
     <div class="map" ref="googleMap"></div>
+
   </div>
+
   <div class="cite">
     <cite>
     「日本の主な山岳一覧」（国土地理院）（https://www.gsi.go.jp/kihonjohochousa/kihonjohochousa41140.html）をもとに作成
@@ -192,17 +276,26 @@ export default defineComponent({
 </template>
 
 <style lang="scss" scoped>
-.searchContent {
-  font-size: 0.8rem;
-}
-
 .mainContent {
   display: flex;
-  padding: 10px;
+}
+
+.searchContent {
+  font-size: 0.8rem;
+  width: 16vw;
+  margin-right: 15px;
+}
+
+.searchBlock {
+  margin-bottom: 20px;
+}
+
+.searchBlock_title {
+  font-weight: bold;
 }
 
 .mountainList {
-  width: 35vw;
+  width: 24vw;
   margin-right: 15px;
 }
 
